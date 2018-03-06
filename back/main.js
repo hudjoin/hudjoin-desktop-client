4ec1aea4c6d75aaad62b642691cda5ca0945a812
Pratik Kumar <pkumar@tavisca.com>
2018-03-06 15:15:06 +0530

    Added windows registry reading example

--- back/main.js
+++ back/main.js
@@ -58,3 +58,18 @@ app.on('window-all-closed', () => {
     server.close()
     app.quit()
 })
+
+var Registry = require('winreg')
+,   regKey = new Registry({                                       // new operator is optional 
+      hive: Registry.HKCU,                                        // open registry hive HKEY_CURRENT_USER 
+      key:  '\\Software\\Microsoft\\Windows\\CurrentVersion\\Run' // key containing autostart programs 
+    })
+ 
+// list autostart programs 
+regKey.values(function (err, items /* array of RegistryItem */) {
+  if (err)
+    console.log('ERROR: '+err);
+  else
+    for (var i=0; i<items.length; i++)
+      console.log('ITEM: '+items[i].name+'\t'+items[i].type+'\t'+items[i].value);
+})
